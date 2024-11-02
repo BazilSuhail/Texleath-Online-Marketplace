9cba0478681d21762e948817d8cf4b2bca119687
bazil-1854 <bscs22072@itu.edu.pk>
2024-11-02 22:34:18 +0500

    updated index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import { Provider } from 'react-redux';
-import store from './redux/store';
+import App from './App'; 
 import reportWebVitals from './reportWebVitals';
+import { CartStateProvider } from './ContextProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode> 
-    <Provider store={store}>
+    <CartStateProvider>
       <App />
-    </Provider>
+    </CartStateProvider>
   </React.StrictMode>
 ); 
 
